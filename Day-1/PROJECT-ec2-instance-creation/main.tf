c115a955257a607b4b7afa2733a621c14bc09bc8
Enoch Gyampoh <136348310+gyenoch@users.noreply.github.com>
2024-07-11 04:00:12 +0000

    main.tf file updated

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,33 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+  region = "us-east-1" # Set your desired AWS region
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+data "aws_subnets" "example" {
+  filter {
+    name   = "tag:Name"
+    values = ["*private*"]
+  }
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+  ami = data.aws_ami.ubuntu.id
+  #ami           = "ami-04a81a99f5ec58529"  # Specify an appropriate AMI ID
+  instance_type = "t2.micro"
+  key_name = "my_first_instance"
 }

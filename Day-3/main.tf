66142c07407549b54de7382899a5993a1dcbcaf0
Enoch Gyampoh <136348310+gyenoch@users.noreply.github.com>
2024-07-12 04:08:20 +0000

    Day-3 folder updated

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 module "ec2_instance" {
-  source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" # replace this
+  source              = "./modules/ec2_instance"
+  ami_value           = "ami-0b72821e2f351e396" # replace this
   instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
+  subnet_id_value     = "subnet-078558c9b90595265"
 }
--- Day-3/modules/ec2_instance/variables.tf
+++ Day-3/modules/ec2_instance/variables.tf
@@ -1,11 +1,13 @@
 variable "ami_value" {
-    description = "value for the ami"
+  description = "value for the ami"
+  type        = string
 }
 
 variable "instance_type_value" {
-    description = "value for instance_type"
+  description = "value for instance_type"
+  type        = string
 }
 
 variable "subnet_id_value" {
-    description = "value for the subnet_id"
+  description = "value for the subnet_id"
 }
